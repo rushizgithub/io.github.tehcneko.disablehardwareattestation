e383ce1970d3696955522453508caa2a985922a0
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-10-18 17:27:28 +0530

    Update GMSHook.java

--- app/src/main/java/io/github/tehcneko/disablehardwareattestation/GMSHook.java
+++ app/src/main/java/io/github/tehcneko/disablehardwareattestation/GMSHook.java
@@ -15,29 +15,22 @@ public class GMSHook implements IXposedHookLoadPackage {
 
     private static final String TAG = GMSHook.class.getSimpleName();
 
-    // Create a HashMap to map package names to spoof information
     private static final Map<String, SpoofInfo> spoofInfoMap = new HashMap<>();
 
-    // Define SpoofInfo class to hold spoof values for a package
     private static class SpoofInfo {
         String brand;
-        String manufacturer;
-        String device;
         String model;
 
-        SpoofInfo(String brand, String manufacturer, String device, String model) {
+        SpoofInfo(String brand, String model) {
             this.brand = brand;
-            this.manufacturer = manufacturer;
-            this.device = device;
             this.model = model;
         }
     }
 
     static {
-        // Populate the spoofInfoMap with package names and their respective spoof information
-        spoofInfoMap.put("ru.andr7e.deviceinfohw", new SpoofInfo("asus", "asus", "AI2201", "ASUS_AI2201"));
-        spoofInfoMap.put("com.ytheekshana.deviceinfo", new SpoofInfo("Sony", "Sony", "Sony", "SO-52A"));
-        spoofInfoMap.put("flar2.devcheck", new SpoofInfo("Xiaomi", "Xiaomi", "2210132C", "2210132C"));
+        spoofInfoMap.put("ru.andr7e.deviceinfohw", new SpoofInfo("asus", "ASUS_AI2201"));
+        spoofInfoMap.put("com.ytheekshana.deviceinfo", new SpoofInfo("Sony", "SO-52A"));
+        spoofInfoMap.put("flar2.devcheck", new SpoofInfo("Xiaomi", "2210132C"));
         // Add more packages and spoof information as needed
     }
 
@@ -51,9 +44,11 @@ public class GMSHook implements IXposedHookLoadPackage {
 
     private void spoofDeviceProperties(SpoofInfo spoofInfo) {
         setBuildField("BRAND", spoofInfo.brand);
-        setBuildField("MANUFACTURER", spoofInfo.manufacturer);
-        setBuildField("DEVICE", spoofInfo.device);
         setBuildField("MODEL", spoofInfo.model);
+
+        // Log the spoofed values for verification
+        Log.i(TAG, "Found" + packageName) ;
+        Log.i(TAG, "Spoofed BRAND: " + spoofInfo.brand " MODEL: " + spoofInfo.model);
     }
 
     private void setBuildField(String key, String value) {
@@ -63,7 +58,7 @@ public class GMSHook implements IXposedHookLoadPackage {
             field.set(null, value);
             field.setAccessible(false);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            Log.e(TAG, "Failed to spoof Build." + key, e);
+            Log.e(TAG, "Failed to spoof app." + key, e);
         }
     }
 }
