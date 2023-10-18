994f8a72371dcf420aa3dee5ef67d1816761f377
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-10-18 18:12:30 +0530

    Update GMSHook.java

--- app/src/main/java/io/github/tehcneko/disablehardwareattestation/GMSHook.java
+++ app/src/main/java/io/github/tehcneko/disablehardwareattestation/GMSHook.java
@@ -5,13 +5,9 @@ import android.os.Build;
 import android.util.Log;
 
 import java.lang.reflect.Field;
-import java.security.KeyStore;
-import java.util.Arrays;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 @SuppressLint("DiscouragedPrivateApi")
@@ -19,48 +15,59 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class GMSHook implements IXposedHookLoadPackage {
 
     private static final String TAG = GMSHook.class.getSimpleName();
+
     private static final String[] PACKAGE_1 = {
-    "com.activision.callofduty.shooter",
-            "com.garena.game.codm",
-            "com.tencent.tmgp.kr.codm",
-            "com.vng.codmvn",
-    "flar2.devcheck"
-};
+        "com.activision.callofduty.shooter",
+        "com.garena.game.codm",
+        "com.tencent.tmgp.kr.codm",
+        "com.vng.codmvn",
+        "flar2.devcheck"
+    };
 
     private static final String[] PACKAGE_2 = {
-    "ru.andr7e.deviceinfohw",
-    "com.activision.callofduty.shooter",
-            "com.garena.game.codm",
-            "com.tencent.tmgp.kr.codm",
-            "com.vng.codmvn"
-};
+        "ru.andr7e.deviceinfohw",
+        "com.activision.callofduty.shooter",
+        "com.garena.game.codm",
+        "com.tencent.tmgp.kr.codm",
+        "com.vng.codmvn"
+    };
 
-        private static final String[] PACKAGE_3 = {
-"com.ea.gp.apexlegendsmobilefps",
-            "com.levelinfinite.hotta.gp",
-            "com.mobile.legends",
-            "com.supercell.clashofclans",
-            "com.tencent.tmgp.sgame",
-            "com.vng.mlbbvn",
-            "com.ytheekshana.deviceinfo"
-            
-};
-
-    private static final String PACKAGE_3 = "flar2.devcheck";
+    private static final String[] PACKAGE_3 = {
+        "com.ea.gp.apexlegendsmobilefps",
+        "com.levelinfinite.hotta.gp",
+        "com.mobile.legends",
+        "com.supercell.clashofclans",
+        "com.tencent.tmgp.sgame",
+        "com.vng.mlbbvn",
+        "com.ytheekshana.deviceinfo"
+    };
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        if (PACKAGE_1.equals(loadPackageParam.packageName)) {
+        spoofPackage(loadPackageParam.packageName);
+    }
+
+    private static void spoofPackage(String packageName) {
+        if (isInPackageArray(PACKAGE_1, packageName)) {
             spoof1();
         }
 
-        if (PACKAGE_2.equals(loadPackageParam.packageName)) {
+        if (isInPackageArray(PACKAGE_2, packageName)) {
             spoof2();
         }
 
-        if (PACKAGE_3.equals(loadPackageParam.packageName)) {
+        if (isInPackageArray(PACKAGE_3, packageName)) {
             spoof3();
         }
+    }
+
+    private static boolean isInPackageArray(String[] packageArray, String packageName) {
+        for (String pkg : packageArray) {
+            if (pkg.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static void spoof1() {
@@ -70,14 +77,14 @@ public class GMSHook implements IXposedHookLoadPackage {
         setBuildField("MODEL", "ASUS_AI2201");
     }
 
-        private static void spoof2() {
+    private static void spoof2() {
         setBuildField("BRAND", "Sony");
         setBuildField("MANUFACTURER", "Sony");
         setBuildField("DEVICE", "Sony");
         setBuildField("MODEL", "SO-52A");
     }
 
-        private static void spoof3() {
+    private static void spoof3() {
         setBuildField("BRAND", "Xiaomi");
         setBuildField("MANUFACTURER", "Xiaomi");
         setBuildField("DEVICE", "2210132C");
@@ -86,16 +93,12 @@ public class GMSHook implements IXposedHookLoadPackage {
 
     private static void setBuildField(String key, String value) {
         try {
-            // Unlock
             Field field = Build.class.getDeclaredField(key);
             field.setAccessible(true);
-            // Edit
             field.set(null, value);
-            // Lock
             field.setAccessible(false);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             Log.e(TAG, "Failed to spoof Build." + key, e);
         }
     }
-
 }
